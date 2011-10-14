fbbeb3eb7b42185d9d872689202a8193a1614de6
Daniel Lewis <daniel@cloudspace.com>
2011-10-13 22:57:30 -0400

    Initial commit to derp_app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DerpApp::Application.config.secret_token = '8c56280a7f98746806a4b5023aaca6c49e5f4412064cc01901aca2416645e6b4ea500a1bd14eaecf9eeb2df7cef9c434830de94657ff3d22a10ec87002a0f398'
